a4e93c1161415fe94c34b307bf349e7333e22972
lolbdf <62429100+lolbdf@users.noreply.github.com>
2020-06-19 08:46:31 +0200

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,13 +1,13 @@
 import generat_random_list
 import time
+import check
 
 
 def selection_sort(list):
     for index in range(len(list)):
         minimum_index = index
         for i in range(minimum_index + 1, len(list)):
-            if ord(list[minimum_index][0].lower()) > ord(list[
-                i][0].lower()):  # min indexmuss größer sein als i in list dann wird true zurück gegeben
+            if check.check_elements(list[minimum_index], list[i]):  # min indexmuss größer sein als i in list dann wird true zurück gegeben
                 minimum_index = i
         cache = list[index]
         list[index] = list[minimum_index]
@@ -18,7 +18,7 @@ def selection_sort(list):
 
 if __name__ == "__main__":
     #unsorted_list = ["Haus", "brot", "Brot", "haus", "boot", "Boot", "hund", "Hund"]
-    unsorted_list = generat_random_list.create(9999)
+    unsorted_list = generat_random_list.create(99)
     start = time.time()
     sortet_list = selection_sort(unsorted_list)
     end = time.time()
@@ -26,4 +26,5 @@ if __name__ == "__main__":
     print(sortet_list)
     print(dauer)
     for i in range(len(sortet_list) - 1):
-        assert sortet_list[i].lower() <= sortet_list[i + 1].lower()
+        assert not check.check_elements(sortet_list[i], sortet_list[i + 1])
+
